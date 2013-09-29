857a68d35c1b83b2f46806cd681ad855905d0e9c
headdetect <headdetective2@gmail.com>
2013-09-29 12:37:47 -0600

    Replaced Matrix with agsXMPP. More work to hipchat support

--- Chatterbox.Hipchat/HipchatChatPlugin.cs
+++ Chatterbox.Hipchat/HipchatChatPlugin.cs
@@ -5,10 +5,13 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Web;
+using System.Web.UI.WebControls;
 using System.Xml;
+using agsXMPP;
+using agsXMPP.protocol.client;
+using agsXMPP.protocol.x.muc;
+using agsXMPP.Xml.Dom;
 using Chatterbox.Plugins;
-using Matrix.Xmpp;
-using Matrix.Xmpp.Client;
 
 namespace Chatterbox.Hipchat
 {
@@ -18,7 +21,11 @@ namespace Chatterbox.Hipchat
         internal static List<string> Users = new List<string>();
         internal static XmlElement LoginData;
 
-        internal static XmppClient HipchatClient;
+        internal static XmppClientConnection HipchatClient;
+        internal static Jid SelfJid;
+        internal static MucManager MucManager;
+
+        internal static LobbyControl Lobby;
 
         #region Hipchat Vars
 
@@ -87,8 +94,12 @@ namespace Chatterbox.Hipchat
 
         #endregion
 
+        internal static Gui.MainWindow Window;
+
         public override void OnLoad(Gui.MainWindow window)
         {
+            Window = window;
+
             var loginWindow = new LoginWindow();
             if (!loginWindow.ShowDialog().HasValue || LoginData == null)
             {
@@ -96,27 +107,43 @@ namespace Chatterbox.Hipchat
                 return;
             }
 
+            SelfJid = new Jid(Nickname + "@chat.hipchat.com");
 
             window.Title = "Chatterbox - " + GroupName;
 
-            HipchatClient = new XmppClient(Nickname, "conf.hipchat.com",  loginWindow.Password);
-            HipchatClient.Open();
+            HipchatClient = new XmppClientConnection("chat.hipchat.com");
+            HipchatClient.Open(Nickname, loginWindow.Password);
             HipchatClient.Status = "DID IT WORK :D?";
-            HipchatClient.Show = Show.chat;
+            HipchatClient.Show = ShowType.chat;
+            HipchatClient.AutoResolveConnectServer = false;
+            HipchatClient.OnLogin += HipchatClient_OnLogin;
+
+            MucManager  = new MucManager(HipchatClient);
 
             HipchatClient.OnPresence += HipchatClient_OnPresence;
+            HipchatClient.OnRosterItem += HipchatClient_OnRosterItem;
 
-            window.SetLobbyRoom(new LobbyControl());
-
-
-            //TODO: Load all rooms and users
+            Window.SetLobbyRoom(Lobby = new LobbyControl());
         }
 
-        void HipchatClient_OnPresence(object sender, PresenceEventArgs e)
+        void HipchatClient_OnRosterItem(object sender, agsXMPP.protocol.iq.roster.RosterItem item)
         {
-            // Users and such //
-            throw new NotImplementedException();
+            Lobby.Dispatcher.Invoke(new Action(() => Lobby.lstRooms.Items.Add("ROST " + item.Name)));
         }
+
+        void HipchatClient_OnLogin(object sender)
+        {
+            MucManager.JoinRoom(new Jid("room@conf.hipchat.com"), Name);
+            //HipchatClient.Send(new Message(new Jid("room@conf.hipchat.com"), MessageType.groupchat, "This is a test message. Pls ignore k?"));
+            
+            
+        }
+
+        void HipchatClient_OnPresence(object sender, Presence pres)
+        {
+            Lobby.Dispatcher.Invoke(new Action(() => Lobby.lstRooms.Items.Add("PRES " + pres.MucUser.Item)));
+        }
+
 
         public override bool OnJoinRoom(string room)
         {
@@ -135,7 +162,7 @@ namespace Chatterbox.Hipchat
             using (var client = new WebClient())
             {
                 client.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
-                var responseString = client.UploadString("https://api-static-2.hipchat.com/api/connect_info", "POST", string.Format(LoginFormat, username, password));
+                var responseString = client.UploadString("https://api-static-1.hipchat.com/api/connect_info", "POST", string.Format(LoginFormat, username, password));
                 if (responseString.Contains("Invalid email or password.")) return;
 
                 var loginDataDocument = new XmlDocument();
